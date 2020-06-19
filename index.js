be9bb8957c8ef7a8cd0d42b2729e6e349437caef
charliekaks <kaksunchained@gmail.com>
2020-06-19 19:01:10 +0300

    Added templating and some basic html and css.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const chalk = require("chalk");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'/public')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-    res.send("Relearning node and express")
+    res.render("index",{title:"Hello World"})
 })
 
 app.listen(3000, ()=>{
