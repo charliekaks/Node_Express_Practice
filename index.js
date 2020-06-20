8bef1cdc9bb0dc27aa61eb4efe95f915176d3567
charliekaks <kaksunchained@gmail.com>
2020-06-20 12:46:25 +0300

    added some basic routing and a books view

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const path = require("path");
 
 const app = express();
 
+const bookRouter = express.Router();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public')));
+
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get((req, res)=>{
+        res.render('books',{nav:[{title:"Books", link:'books'},{title:'Author', link:'author'}]});
+    })
+
+app.use('/books',bookRouter);
 app.get('/', (req, res)=>{
-    res.render("index",{title:"Hello World"})
+    res.render("index",{nav:[{title:"Books", link:'books'},{title:'Author', link:'author'}]})
 })
 
 app.listen(3000, ()=>{
